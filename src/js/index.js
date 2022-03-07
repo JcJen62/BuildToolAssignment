23446417b522fee75e0c2f2b73c6387abe685f99
jcjen62 <jcjensen62@gmail.com>
2022-03-06 17:30:41 -0700

    Todo app complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 let todos = [
     {
         "title": "Eat",
@@ -17,21 +16,49 @@ let todos = [
       }
 ]
 
+const deleteTodos = document.querySelector('#deleteBtn')
+deleteTodos.addEventListener('click', () => {
+    for (let i = todos.length - 1; i >= 0; i--) {
+        let todo = todos[i]
+          if(todo.complete === true){
+            todos.splice(i, 1)
+          }
+      }
+
+      let containerDiv = document.querySelector('#todoContainer')
+      containerDiv.innerHTML = ""
+      displayTodos(todos)
+})
+
 const addItemBtn = document.querySelector("#addBtn");
 addItemBtn.addEventListener("click", () => {
   let newTask = {
     title: document.querySelector("#addText").value,
-    category: "",
-    id: 0,
+    id: todos.length + 1,
     complete: false
   }
 
-  todos.push(...newTask)
+  todos.push(newTask)
+
+  let containerDiv = document.querySelector('#todoContainer')
+  containerDiv.innerHTML = ""
+  displayTodos(todos)
 })
 
 let containerDiv = document.querySelector('#todoContainer')
 const displayTodos = (arr) => {
     arr.forEach(element => {
+        const editInput = document.createElement('input')
+        editInput.setAttribute('id', 'newTitle')
+        editInput.setAttribute('type', 'text')
+        editInput.setAttribute('placeholder', element.title)
+
+        const saveNewTitle = document.createElement('a')
+        saveNewTitle.setAttribute('href', '#')
+        saveNewTitle.classList.add('card-link')
+        saveNewTitle.setAttribute('id', `${element.id}`)
+        saveNewTitle.innerHTML = 'Save Note'
+
         const cardDiv = document.createElement("div")
         cardDiv.classList.add('card')
 
@@ -42,6 +69,11 @@ const displayTodos = (arr) => {
         cardTitle.classList.add('card-title')
         cardTitle.innerHTML = element.title
         cardBody.appendChild(cardTitle)
+
+        const cardDone = document.createElement('p')
+        cardDone.classList.add('card-text')
+        cardDone.innerHTML = ''
+        cardBody.appendChild(cardDone)
         
         const editBtn = document.createElement('a')
         editBtn.classList.add('card-link')
@@ -49,11 +81,31 @@ const displayTodos = (arr) => {
         editBtn.innerHTML = 'Edit ToDo'
         cardBody.appendChild(editBtn)
 
+        editBtn.addEventListener('click', () => {
+            cardTitle.replaceWith(editInput)
+            editBtn.replaceWith(saveNewTitle)
+        })
+
+        saveNewTitle.addEventListener('click', (event) => {
+            elemIndex = todos.findIndex((obj) => obj.id === element.id)
+            todos[elemIndex].title = editInput.value;
+            
+            containerDiv.innerHTML = ''
+            displayTodos(todos)
+        })
+
         const completeBtn = document.createElement('a')
         completeBtn.classList.add('card-link')
+        completeBtn.classList.add('complete')
         completeBtn.setAttribute('id', element.id)
         completeBtn.innerHTML = 'Complete ToDo'
         cardBody.appendChild(completeBtn)
+
+        completeBtn.addEventListener('click', (event) => {
+            cardDone.innerHTML = 'Completed! &#x2713'
+            elemIndex = todos.findIndex((obj) => obj.id === element.id)
+            todos[elemIndex].complete = true
+        })
 
         cardDiv.appendChild(cardBody)
         containerDiv.appendChild(cardDiv)
